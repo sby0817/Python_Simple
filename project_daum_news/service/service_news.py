99c5a9e56125c169c4f6808ead658cbbbccdb29e
sby0817 <134240452+sby0817@users.noreply.github.com>
2023-11-10 11:57:57 +0800

    update service_news.py

--- project_daum_news/service/service_news.py
+++ project_daum_news/service/service_news.py
@@ -1,11 +1,10 @@
-# 해당 URL 접속해서 기사 [제목, 본문, 날짜] 수집
+# 해당 URL 접속해서 기사  [제목, 본문, 날짜] 수집
 import requests
 from bs4 import BeautifulSoup
 
 
 def get_news(url: str):
     result = requests.get(url)
-
     doc = BeautifulSoup(result.text, "html.parser")
 
     reg_date = doc.select("span.num_date")[0].get_text()
@@ -15,9 +14,7 @@ def get_news(url: str):
     print(f"제목: {title}")
 
     content_list = doc.select("div.article_view p")
-
     content = ""
     for p in content_list:
         content += p.get_text()
-
     print(f"본문: {content}")
