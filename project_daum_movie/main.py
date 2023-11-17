7ea01190f70ae8f61763c6352a30fa88b584e175
sby0817 <134240452+sby0817@users.noreply.github.com>
2023-11-17 13:28:56 +0800

    Update main.py

--- project_daum_movie/main.py
+++ project_daum_movie/main.py
@@ -16,6 +16,7 @@
 #   - URL: https://sites.google.com/chromium.org/driver/
 #  2. 실시간(코드) 다운로드
 
+from datetime import datetime, timedelta
 import math
 import re
 import time
@@ -32,7 +33,7 @@ options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),
                           options=options)
 # 2. URL 접속
-url = "https://movie.daum.net/moviedb/grade?movieId=165591"
+url = "https://movie.daum.net/moviedb/grade?movieId=169137"
 driver.get(url)
 time.sleep(2)
 
@@ -78,4 +79,33 @@ for i in range(click_cnt):
 # 8. 전체 소스 코드 가져오기
 doc_html = driver.page_source
 doc = BeautifulSoup(doc_html, "html.parser")
-review_list = doc.select("")
+review_list = doc.select("ul.list_comment > li")
+print(f"= 전체 리뷰: {len(review_list)}건")
+
+# item: 리뷰 1건(평점, 리뷰, 작성자, 작서일자)
+for item in review_list:
+    print("=" * 100)
+    review_score = item.select("div.ratings")[0].get_text()
+    print(f"  - 평점: {review_score}")
+
+    review_content = item.select("p.desc_txt")[0].get_text().strip()
+    # \n : 한 줄 개행 -> \n을 제거
+    review_content = re.sub("\n", "", review_content)
+    print(f"  - 리뷰: {review_content}")
+
+    review_writer = item.select("a.link_nick > span")[1].get_text()  # [1].get_text()를 안 하고 [0]으로 하면 [댓글작성자, 작성자, 댓글 모아보기]가 다 나옴
+    print(f"  - 작성자: {review_writer}")
+
+    review_date = item.select("span.txt_date")[0].get_text()
+    # 24시간 이내에 작성된 리뷰의 날짜 -> 24시간 전, 3시간 전 -> 다음 영화 날짜(2023. 11. 17. 2:12)
+    # 1) 24시간 전, 17시간 전과 같은 날짜 찾기
+    if len(review_date) < 7:
+        # 2) "17시간 전" -> 숫자만 추출 17
+        reg_hour = int(re.sub(r"[^~0-9]", "", review_date))
+        # 3) 등록일자 = 현재 시간 - 17
+        # print(f"현재 시간: {datetime.now()}")  # 년 월 일 시 분 초 나노초
+        review_date = datetime.now() - timedelta(hours = reg_hour)
+        # print(f"등록 시간: {review_date}")
+        # 4) 계산된 등록 일자 날짜 포맷 변경(다음 영화 리뷰 날짜 포맷)
+        review_date = review_date.strftime("%Y. %m. %d. %H:%M")
+    print(f"  - 날짜: {review_date}")
